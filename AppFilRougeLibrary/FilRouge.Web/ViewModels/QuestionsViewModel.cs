1f18a2d2ed8df75021f03c2fe20b908f7e789dc1
Administrateur <Administrateur@M2IPORT-9TLQNC2>
2018-03-30 11:33:43 +0200

    Création des fichiers ViewModels

---
+++ AppFilRougeLibrary/FilRouge.Web/ViewModels/QuestionsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilRouge.Web.ViewModels
+{
+	public class QuestionsViewModel
+	{
+	}
+}
---
+++ AppFilRougeLibrary/FilRouge.Web/ViewModels/TypeQuestionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FilRouge.Web.ViewModels
+{
+	public class TypeQuestionViewModel
+	{
+		public int Id { get; set; }
+		[Required]
+		[Display(Name = "Nom étudiant : ")]
+		public string Name { get; set; }
+		public int TypeQuestionId { get; set; }
+		public string NameType { get; set; }
+	}
+}
