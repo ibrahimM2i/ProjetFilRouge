49a9de039b4fb88e414bc432de4f6c1922960a7c
Abdoulaye Sow <sowabdoulaye@outlook.com>
2018-04-03 10:09:12 +0200

    création pour Contact : controller, vue, service,viewmodel,map

---
+++ AppFilRougeLibrary/FilRouge.Web/Controllers/ContactController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FilRouge.Web.Services;
+using FilRouge.Web.ViewModels;
+
+namespace FilRouge.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactService _contactService = new ContactService();
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var contacts = _contactService.GetAllContact();
+            return View(contacts);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new ContactViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(ContactViewModel contactViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactService.AddContact(contactViewModel);
+                return RedirectToAction("Index", "Contact");
+            }
+            return View(contactViewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ContactViewModel contactViewModel = _contactService.GetContactById(id);
+            return View(contactViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ContactViewModel contactViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactService.EditContact(contactViewModel);
+                return RedirectToAction("Index", "Contact");
+            }
+            return View(contactViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            ContactViewModel contactViewModel = _contactService.GetContactById(id);
+            return View(contactViewModel);
+        }
+
+    }
+}
